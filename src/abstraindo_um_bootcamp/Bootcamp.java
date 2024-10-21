f3977e46c2b96b57ef3239f7653799847f4732ba
Alexandre liberato <alexandreliberatto@gmail.com>
2024-10-21 11:27:28 -0300

    Iplementando classe Bootcamp
    Signed-off-by: Alexandre liberato <alexandreliberatto@gmail.com>

--- src/abstraindo_um_bootcamp/Bootcamp.java
+++ src/abstraindo_um_bootcamp/Bootcamp.java
@@ -1,122 +1,57 @@
 package abstraindo_um_bootcamp;
 
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+
 public class Bootcamp {
 
-	//Atributos
-	private String nome;
-	private String descricao;
-	private LocalDate dataInicio;
-	private LocalDate dataFim;
-	private Set<Conteudo> conteudos = new HashSet<>();
-	private Set<Devs> devs = new HashSet<>();
-	
-	
-	public Bootcamp(String nome, String descricao, LocalDate dataInicio, LocalDate dataFim, Set<Conteudo> conteudos,
-			Set<Devs> devs) {
-		super();
-		this.nome = nome;
-		this.descricao = descricao;
-		this.dataInicio = dataInicio;
-		this.dataFim = dataFim;
-		this.conteudos = conteudos;
-		this.devs = devs;
-	}
-	
-	 // Método para adicionar conteudos
-    public static void adicionarConteudo(Set<Conteudo> conteudos, String conteudo) {
-        if (conteudos.add(conteudo)) {
-            System.out.println("Elemento '" + conteudo + "' adicionado com sucesso.");
-        } else {
-            System.out.println("Elemento '" + conteudo + "' já existe no conjunto.");
+    private String nome;
+    private String descricao;
+    private LocalDate dataInicio;
+    private LocalDate dataFim;
+    private Set<Conteudo> conteudos = new HashSet<>();
+    private Set<Dev> devsInscritos = new HashSet<>();
+
+    public Bootcamp(String nome, String descricao) {
+        this.nome = nome;
+        this.descricao = descricao;
+        this.dataInicio = LocalDate.now();
+        this.dataFim = dataInicio.plusDays(45); // Bootcamp com duração padrão de 45 dias
+    }
+
+    public void adicionarConteudo(Conteudo conteudo) {
+        conteudos.add(conteudo);
+    }
+
+    public void listarConteudos() {
+        System.out.println("Conteúdos do Bootcamp:");
+        for (Conteudo c : conteudos) {
+            System.out.println("- " + c.getTitulo());
         }
     }
-    
-    //Método para listar conteúdo
-    public void listarConteúdo(Set<Conteudo> conteudos) {
-    	System.out.println("Conteudos do Bootcamp:");
-    	for(Conteudo i : conteudos) {
-    		System.out.println("- "+ i);
-    	}
+
+    public void adicionarDev(Dev dev) {
+        devsInscritos.add(dev);
     }
 
-    // Método para adicionar devs
-    public static void adicionarConteudo(Set<Devs> devs, String dev) {
-        if (devs.add(dev)) {
-            System.out.println("Elemento '" + dev + "' adicionado com sucesso.");
-        } else {
-            System.out.println("Elemento '" + dev + "' já existe no conjunto.");
+    public void listarDevs() {
+        System.out.println("Devs inscritos no Bootcamp:");
+        for (Dev dev : devsInscritos) {
+            System.out.println("- " + dev.getNome());
         }
     }
-    
-    //Método para listar devs
-    public void listarConteúdo(Set<Devs> devs) {
-    	System.out.println("Devs do Bootcamp:");
-    	for(Devs i : devs) {
-    		System.out.println("- "+ i);
-    	}
-    }
 
-	//Geters e Seters
-	public String getNome() {
-		return nome;
-	}
+    // Getters e Setters
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
 
+    public String getDescricao() { return descricao; }
+    public void setDescricao(String descricao) { this.descricao = descricao; }
 
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
+    public LocalDate getDataInicio() { return dataInicio; }
+    public LocalDate getDataFim() { return dataFim; }
 
-
-	public String getDescricao() {
-		return descricao;
-	}
-
-
-	public void setDescricao(String descricao) {
-		this.descricao = descricao;
-	}
-
-
-	public LocalDate getDataInicio() {
-		return dataInicio;
-	}
-
-
-	public void setDataInicio(LocalDate dataInicio) {
-		this.dataInicio = dataInicio;
-	}
-
-
-	public LocalDate getDataFim() {
-		return dataFim;
-	}
-
-
-	public void setDataFim(LocalDate dataFim) {
-		this.dataFim = dataFim;
-	}
-
-
-	public Set<Conteudo> getConteudos() {
-		return conteudos;
-	}
-
-
-	public void setConteudos(Set<Conteudo> conteudos) {
-		this.conteudos = conteudos;
-	}
-
-
-	public Set<Devs> getDevs() {
-		return devs;
-	}
-
-
-	public void setDevs(Set<Devs> devs) {
-		this.devs = devs;
-	}
-	
-	
-	
-	
+    public Set<Conteudo> getConteudos() { return conteudos; }
+    public Set<Dev> getDevsInscritos() { return devsInscritos; }
 }
--- src/abstraindo_um_bootcamp/Mentorias.java
+++ src/abstraindo_um_bootcamp/Mentorias.java
@@ -1,5 +1,30 @@
 package abstraindo_um_bootcamp;
 
-public class Mentorias {
+public class Mentorias extends Conteudo {
+
+    //Atributo
+    private LocalDate data;
+
+
+    @Override
+    public double calcularXp(double xp_padrao) {
+        double total_xp = 0;
+        for(int i = 0; i <= cargaHoraria.length; i++){
+           total_xp += xp_padrao;
+        }
+        return total_xp;
+    }
+
+
+    public LocalDate getData() {
+        return data;
+    }
+
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+
+    
 
 }
