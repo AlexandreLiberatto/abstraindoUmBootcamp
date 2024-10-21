ee94adc09ef98ed2ab8930bb4bb80dd3b6d8798b
Alexandre liberato <alexandreliberatto@gmail.com>
2024-10-21 11:28:16 -0300

    Iplementando classe Conteudo
    Signed-off-by: Alexandre liberato <alexandreliberatto@gmail.com>

--- src/abstraindo_um_bootcamp/Conteudo.java
+++ src/abstraindo_um_bootcamp/Conteudo.java
@@ -1,39 +1,17 @@
 package abstraindo_um_bootcamp;
 
-abstract class Conteudo {
+public abstract class Conteudo {
 
-    //Atributos
     private String titulo;
     private String descricao;
-    final double xp_padrao;
+    protected static final double XP_PADRAO = 10.0;
 
+    public abstract double calcularXp();
 
-    public Conteudo(String titulo, String descricao, double xp_padrao){
-        this.titulo = titulo;
-        this.descricao = descricao;
-        this.xp_padrao = xp_padrao;
-    }
+    // Getters e Setters
+    public String getTitulo() { return titulo; }
+    public void setTitulo(String titulo) { this.titulo = titulo; }
 
-    public abstract double calcularXp(double xp_padrao){
-        
-    }
-
-    public String getTitulo() {
-        return titulo;
-    }
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-    public String getDescricao() {
-        return descricao;
-    }
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
-    public double getXp_padrao() {
-        return xp_padrao;
-    }
-
-    
-
+    public String getDescricao() { return descricao; }
+    public void setDescricao(String descricao) { this.descricao = descricao; }
 }
