d1e47451ce6056d24e005eb84935e0458e9a9863
Alexandre liberato <alexandreliberatto@gmail.com>
2024-10-21 11:29:06 -0300

    Iplementando classe Cursos
    Signed-off-by: Alexandre liberato <alexandreliberatto@gmail.com>

--- src/abstraindo_um_bootcamp/Cursos.java
+++ src/abstraindo_um_bootcamp/Cursos.java
@@ -2,34 +2,20 @@ package abstraindo_um_bootcamp;
 
 public class Cursos extends Conteudo {
 
-    //Atributo
     private int cargaHoraria;
 
-    public Cursos(int cargaHoraria){
+    public Cursos(String titulo, String descricao, int cargaHoraria) {
+        setTitulo(titulo);
+        setDescricao(descricao);
         this.cargaHoraria = cargaHoraria;
     }
-
-
-
-    public int getCargaHoraria() {
-        return cargaHoraria;
-    }
-
-    public void setCargaHoraria(int cargaHoraria) {
-        this.cargaHoraria = cargaHoraria;
-    }
-
-
 
     @Override
-    public double calcularXp(double xp_padrao) {
-        double total_xp = 0;
-        for(int i = 0; i <= cargaHoraria.length; i++){
-           total_xp += xp_padrao;
-        }
-        return total_xp;
+    public double calcularXp() {
+        return XP_PADRAO * cargaHoraria;
     }
 
-    
-
+    // Getters e Setters
+    public int getCargaHoraria() { return cargaHoraria; }
+    public void setCargaHoraria(int cargaHoraria) { this.cargaHoraria = cargaHoraria; }
 }
