31a7eb34ffbedbac538ade9466c030c8b902c705
Alexandre liberato <alexandreliberatto@gmail.com>
2024-10-21 11:32:11 -0300

    Main
    Signed-off-by: Alexandre liberato <alexandreliberatto@gmail.com>

--- src/abstraindo_um_bootcamp/Main.java
+++ src/abstraindo_um_bootcamp/Main.java
@@ -1,6 +1,23 @@
 package abstraindo_um_bootcamp;
 
-public class Main {
-	
+import java.time.LocalDate;
 
+public class Main {
+    public static void main(String[] args) {
+        Cursos cursoJava = new Cursos("Java Básico", "Descrição do curso Java", 8);
+        Mentorias mentoriaJava = new Mentorias("Mentoria Java", "Descrição da mentoria", LocalDate.now());
+
+        Bootcamp bootcamp = new Bootcamp("Bootcamp Java Developer", "Descrição do bootcamp");
+        bootcamp.adicionarConteudo(cursoJava);
+        bootcamp.adicionarConteudo(mentoriaJava);
+
+        Dev devJoao = new Dev();
+        devJoao.setNome("João");
+        devJoao.inscreverBootcamp(bootcamp);
+
+        System.out.println("Conteúdos Inscritos João: " + devJoao.getConteudosInscritos());
+        devJoao.progredir();
+        System.out.println("Conteúdos Concluídos João: " + devJoao.getConteudosConcluidos());
+        System.out.println("XP Total: " + devJoao.calcularTotalXp());
+    }
 }
